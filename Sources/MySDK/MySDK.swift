cb5deb7219dfb93181de5abddd30a7f658a0321a
Suneesh <suneesh@Suneeshs-MacBook-Air.local>
2025-06-13 18:02:06 +0530

    test2

--- Sources/MySDK/MySDK.swift
+++ Sources/MySDK/MySDK.swift
@@ -4,28 +4,42 @@ import UIKit
 public class GreetingView: UIView {
     
     private let label = UILabel()
+    private var currentName: String
     
     public init(name: String, frame: CGRect = .zero) {
+        self.currentName = name
         super.init(frame: frame)
-        setupUI(name: name)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        self.currentName = ""
+        super.init(coder: coder)
+        setupUI()
     }
 
-    private func setupUI(name: String) {
+    private func setupUI() {
         backgroundColor = .systemTeal
-        label.text = "Hello, \(name)!"
+        layer.cornerRadius = 12
+        
+        label.text = "Hello, \(currentName)!"
         label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textAlignment = .center
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         
         addSubview(label)
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             label.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
+    }
+    
+    public func updateName(_ name: String) {
+        currentName = name
+        label.text = "Hello, \(name)!"
     }
 }
 #endif
