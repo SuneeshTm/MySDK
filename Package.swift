e2126af00905d51b822f66bccebde386ab42a134
Suneesh <suneesh@Suneeshs-MacBook-Air.local>
2025-06-13 17:09:32 +0530

    testing

--- Package.swift
+++ Package.swift
@@ -1,22 +1,22 @@
-// swift-tools-version:5.7
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "MySDK",
     platforms: [
-        .iOS(.v12) 
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "MySDK",
-            targets: ["MySDK"]
-        ),
+            targets: ["MySDK"]),
     ],
     targets: [
         .target(
             name: "MySDK",
-            dependencies: [],
-            path: "Sources"
-        )
+            dependencies: []),
+        .testTarget(
+            name: "MySDKTests",
+            dependencies: ["MySDK"]),
     ]
 )
